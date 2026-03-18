4e7ac8e7ac1a01e6cf732517ad3f674fc87ef57d
Ali Bidhendi <abidhendi1@utdallas.edu>
2026-03-17 19:51:31 -0500

    Adjust Electron window sizing

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1540,
     height: 1040,
-    minWidth: 1100,
-    minHeight: 760,
+    minWidth: 1180,
+    minHeight: 800,
     backgroundColor: "#0A0A0F",
     autoHideMenuBar: true,
     title: "GrammarChecker",
@@ -24,14 +24,10 @@ app.whenReady().then(() => {
   createWindow();
 
   app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
 app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
-  }
+  if (process.platform !== "darwin") app.quit();
 });
